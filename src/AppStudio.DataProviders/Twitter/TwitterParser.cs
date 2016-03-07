df53a3495314527f3dc53ae6a399ebd695790291
pellea <adrien.pellegrini@gmail.com>
2016-03-07 16:42:35 +0100

    replace shortened URLs by the display URL in the twitter Text property

--- src/AppStudio.DataProviders/Twitter/TwitterParser.cs
+++ src/AppStudio.DataProviders/Twitter/TwitterParser.cs
@@ -29,7 +29,6 @@ namespace AppStudio.DataProviders.Twitter
             TwitterSchema tweet = new TwitterSchema
             {
                 _id = item.Id,
-                Text = item.Text.DecodeHtml(),
                 CreationDateTime = TryParse(item.CreatedAt)
             };
             
@@ -53,6 +52,34 @@ namespace AppStudio.DataProviders.Twitter
                     tweet.UserProfileImageUrl = tweet.UserProfileImageUrl.Replace("_normal", string.Empty);
                 }
             }
+
+            var text = item.Text;
+            if (item.Entities?.Urls?.Count > 0)
+            {
+                foreach (TwitterUrl url in item.Entities.Urls)
+                {
+                    // special case where the URL is a Twitter video
+                    if (url.DisplayUrl.Contains("amp.twimg.com/v/"))
+                    {
+                        text = text.Replace(url.Url, string.Empty);
+                    }
+                    else
+                    {
+                        text = text.Replace(url.Url, url.DisplayUrl);
+                    }
+                }
+            }
+            
+            if (item.Entities?.Media?.Count > 0)
+            {
+                foreach (TwitterMedia media in item.Entities.Media)
+                {
+                    text = text.Replace(media.Url, string.Empty);
+                }
+            }
+
+            // a '\n' could be at thye end of the tweet if we removed a Twitter video
+            tweet.Text = text.TrimEnd('\n').DecodeHtml();
 
             return tweet;
         }
@@ -79,7 +106,6 @@ namespace AppStudio.DataProviders.Twitter
             }
 
             var result = JsonConvert.DeserializeObject<TwitterTimelineItem[]>(data);
-
             return result.Select(r => r.Parse()).ToList();
         }
     }
@@ -97,7 +123,126 @@ namespace AppStudio.DataProviders.Twitter
 
         [JsonProperty("user")]
         public TwitterUser User { get; set; }
+
+        [JsonProperty("entities")]
+        public TwitterEntity Entities { get; set; }
     }
+
+    public class TwitterEntity
+    {
+        [JsonProperty("hashtags")]
+        public List<TwitterHashtags> Hashtags { get; set; }
+
+        [JsonProperty("user_mentions")]
+        public List<TwitterUserMention> UserMentions { get; set; }
+
+        [JsonProperty("urls")]
+        public List<TwitterUrl> Urls { get; set; }
+
+        [JsonProperty("media")]
+        public List<TwitterMedia> Media { get; set; }
+    }
+
+    public class TwitterHashtags
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("indices")]
+        public int[] Indices { get; set; }
+    }
+
+    public class TwitterUserMention
+    {
+        [JsonProperty("screen_name")]
+        public string ScreenName { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("id_str")]
+        public string IdStr { get; set; }
+
+        [JsonProperty("indices")]
+        public int[] Indices { get; set; }
+    }
+
+    public class TwitterUrl
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("expanded_url")]
+        public string ExpandedUrl { get; set; }
+
+        [JsonProperty("display_url")]
+        public string DisplayUrl { get; set; }
+
+        [JsonProperty("indices")]
+        public int[] indices { get; set; }
+    }
+
+    public class TwitterSize
+    {
+        [JsonProperty("w")]
+        public int Width { get; set; }
+
+        [JsonProperty("h")]
+        public int Height { get; set; }
+
+        [JsonProperty("resize")]
+        public string Resize { get; set; }
+    }
+
+    public class Sizes
+    {
+        [JsonProperty("small")]
+        public TwitterSize Small { get; set; }
+
+        [JsonProperty("medium")]
+        public TwitterSize Medium { get; set; }
+
+        [JsonProperty("large")]
+        public TwitterSize Large { get; set; }
+    }
+
+    public class TwitterMedia
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("id_str")]
+        public string IdStr { get; set; }
+
+        [JsonProperty("indices")]
+        public int[] Indices { get; set; }
+
+        [JsonProperty("media_url")]
+        public string MediaUrl { get; set; }
+
+        [JsonProperty("media_url_https")]
+        public string MediaUrlHttps { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("display_url")]
+        public string DisplayUrl { get; set; }
+
+        [JsonProperty("expanded_url")]
+        public string ExpandedUrl { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("sizes")]
+        public Sizes Sizes { get; set; }
+    }
+
+ 
 
     public class TwitterUser
     {
